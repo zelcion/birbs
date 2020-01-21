b667c861a41e4cc902fe1128899c5ef97506d560
Fábio Luis Gomes de Meneses Júnior <fabio@vizir.com.br>
2020-01-20 23:27:47 -0300

    fix exporting

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { Context } from './context/context';
 import { EventManager } from './manager/manager';
+import { Pipeline } from './procedure/pipeline';
 import { Procedure } from './procedure/procedure';
 import { toNewEffect } from './utils/utils';
 
-const utils = {
+export const utils = {
   toNewEffect: toNewEffect
 };
 
-const birbsModule = {
+export const birbsModule = {
   Context,
   EventManager,
+  Pipeline,
   Procedure,
   utils
 };
-
-module.exports = birbsModule;
